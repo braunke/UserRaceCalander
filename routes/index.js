0429085f8f6f2253a0f7da691efb2d80333e0dc5
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-11-01 14:23:26 -0500

    shows database results

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,28 @@ const pg = require('pg');
 const conString = 'postgres://kayla:Hank@localhost/postgres';
 var pool = new pg.Pool();
 
-pool.connect(function (err, client, done) {
-
-    if (err)
-        console.log(err);
-
-    client.query('SELECT * FROM users', function(error, result) {
-        for (var i = 0; i < result.rows.length; i++) {
-            console.log(result.rows[i]);
-        }
-    });
-
-});
-pool.end();
-
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
-
-
+router.get('/userHome', function(req, res, next) {
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log("not able to get connection " + err);
+            res.status(400).send(err);
+        }
+        client.query('SELECT * FROM users', function (error, result) {
+            for (var i = 0; i < result.rows.length; i++) {
+                console.log(result.rows[i]);
+            }
+            if (err) {
+                console.log(err);
+                res.status(400).send(err);
+            }
+            res.status(200).send(result.rows);
+        });
+    });
+});
 
 
 module.exports = router;
