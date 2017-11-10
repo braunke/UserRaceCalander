73bcf7f24f7b33a7c51fd3ce2aaad215dca55fcb
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-11-10 15:53:19 -0600

    more calendar work

---
+++ public/calendar.js
@@ -0,0 +1,51 @@
+
+
+$(document).ready(function() {
+
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,basicWeek,basicDay'
+        },
+        defaultDate: '2017-10-12',
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: [
+            {
+                title: 'Reindeer Run',
+                description: 'This is a cool event',
+                start: '2017-12-11',
+                url: 'https://www.reindeerrun.com/'
+            },
+            {
+                title: 'Long Event',
+                start: '2017-10-07',
+                end: '2017-10-10'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2017-10-09T16:00:00'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2017-10-16T16:00:00'
+            }
+        ],
+        eventClick: function(event){
+            if (event.url){
+                window.open(event.url);
+                return false;
+            }
+        },
+        eventRender: function(event, element) {
+            element.find('.fc-title').append("<br/>" + event.description);
+        }
+
+    });
+
+});
+
